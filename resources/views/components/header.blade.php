6810a66c370a169be283861724a9f5888f4f5a43
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-12-22 09:58:12 +0100

    Styled

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,6 +1,6 @@
 <div>
    @if (Session::has('response'))
-       {{ Session::get('response') }}
+       <h2 style="font-style: italic; font-weight: bold; color:blue">{{ Session::get('response') }}</h2>
    @endif
 
     <hr/>
