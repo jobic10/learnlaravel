beaa36ec4b72f6aa41cad4b8ef6790c2b92c6eed
Owonubi Job Sunday <jobowonubi@gmail.com>
2020-12-22 14:26:25 +0100

    Table Migrated

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-
+use App\Models\Student;
 class StudentController extends Controller
 {
     /**
---
+++ database/migrations/2020_12_22_125048_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('lastname');
+            $table->string('firstname');
+            $table->string('regno')->unique();
+            $table->integer('cpu_id')->default(mt_rand(1,8));
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
