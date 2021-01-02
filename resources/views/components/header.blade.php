2b08233009ee37d96668e0ab5d2fb93df85482d9
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-02 22:26:18 +0100

    Toastr

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,8 @@
-<div>
-   @if (Session::has('response'))
-       <h2 style="font-style: italic; font-weight: bold; color:blue">{{ Session::get('response') }}</h2>
-   @endif
-
-    <hr/>
-</div>
+@if (Session::has('response'))
+   @section('bottomScript')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous"></script>
+    <script>
+        toastr.success("{!! Session::get('response') !!}");
+    </script>
+@endsection
+@endif
--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -4,7 +4,6 @@
 @endsection
 @section('title', ':: Create Student ::')
 @section('script')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdn.tiny.cloud/1/zcoalni64rhfm3hfq7idsdny6d5cpb1da3w44l1ndlow8usl/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 @endsection
