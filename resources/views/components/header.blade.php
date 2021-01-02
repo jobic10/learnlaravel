63c638ad5638bdf9de0356527f84ee74dacf1587
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-02 22:30:23 +0100

    Sweet Alert Success GIF

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,7 +4,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous"></script>
     <script>
         toastr.success("{!! Session::get('response') !!}");
-        swal("Laravel", "{{ Session::get('response') }}");
+        swal("Laravel", "{{ Session::get('response') }}", 'success');
     </script>
 @endsection
 @endif
