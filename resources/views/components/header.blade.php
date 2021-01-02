883530c5055979e9717b09aecc84123cb6ee5ec8
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-02 22:30:03 +0100

    SweetAlert

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,8 +1,10 @@
 @if (Session::has('response'))
    @section('bottomScript')
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous"></script>
     <script>
         toastr.success("{!! Session::get('response') !!}");
+        swal("Laravel", "{{ Session::get('response') }}");
     </script>
 @endsection
 @endif
