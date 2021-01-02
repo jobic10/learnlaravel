b738cd790bcdfc311dae953979aba47a82cf7d08
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-02 22:01:15 +0100

    Added CDNs

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -1,6 +1,10 @@
 @extends('main')
+@section('style')
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.css" integrity="sha512-3pIirOrwegjM6erE5gPSwkUzO+3cTjpnV9lexlNZqvupR64iZBnOOTiiLPb9M36zpMScbmUNIcHUqKD47M719g==" crossorigin="anonymous" />
+@endsection
 @section('title', ':: Create Student ::')
 @section('script')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdn.tiny.cloud/1/zcoalni64rhfm3hfq7idsdny6d5cpb1da3w44l1ndlow8usl/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 @endsection
