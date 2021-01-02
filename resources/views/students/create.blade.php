dfbb2dc2f9c209c3c25f1086df8654f39277f39b
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-02 13:44:33 +0100

    Preview Image

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -1,6 +1,8 @@
 @extends('main')
 @section('title', ':: Create Student ::')
 @section('script')
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
 <script src="https://cdn.tiny.cloud/1/zcoalni64rhfm3hfq7idsdny6d5cpb1da3w44l1ndlow8usl/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
 @endsection
 @section('content')
@@ -22,10 +24,12 @@
     <span style="color: red"> {{ $message }}</span>
 
     @enderror <br>
-    <input type="file" name="passport" id="">@error('passport')
+    <input type="file" onchange="previewFile(this)" name="passport" id="">@error('passport')
     <span style="color: red"> {{ $message }}</span>
 
     @enderror <br>
+    <img id="preview">
+    <br>
     <textarea name="address" id="" cols="30" rows="10"></textarea><br>
     <input type="submit" value="Submit">
 
@@ -46,5 +50,16 @@
       tinycomments_mode: 'embedded',
       tinycomments_author: 'Author name',
    });
+   function previewFile(input){
+       var file = $("input[type=file]").get(0).files[0];
+       console.log(file, "Hi World");
+       if (file){
+           var reader = new FileReader();
+           reader.onload = function (){
+               $("#preview").attr("src",reader.result);
+           }
+           reader.readAsDataURL(file);
+       }
+   }
     </script>
 @endsection
