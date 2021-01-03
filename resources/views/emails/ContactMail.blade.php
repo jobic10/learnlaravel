7e8cc980740757dbd6cfeecccff416864d595f73
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-03 20:38:45 +0100

    Extend to main

--- resources/views/emails/ContactMail.blade.php
+++ resources/views/emails/ContactMail.blade.php
@@ -1,15 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Message Recieved</title>
-</head>
-<body>
+@extends('main')
+@section('title', ':: New Contact Us Form Message::')
+
+@section('content')
 <h3>New Message --- Contact Us</h3>
 <p>Name : {{ $details['name'] }}</p>
 <p>Email : {{ $details['email'] }}</p>
 <p>Message : {{ $details['message'] }}</p>
 </body>
 </html>
+
+@endsection
