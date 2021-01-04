3475c3c2650756633b176715ee74a0269e8599e4
Owonubi Job Sunday <jobowonubi@gmail.com>
2021-01-04 17:58:20 +0100

    Created Factory

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Student;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class StudentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Student::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'firstname' => $this->faker->firstName(),
+            'lastname' => $this->faker->lastName(),
+            'regno' => $this->faker->numberBetween(1,18282),
+        ];
+    }
+}
